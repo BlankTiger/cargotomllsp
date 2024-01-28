39b4c52d8481ebd14dcf795814330c60951feeac
Maciej Urban <maciej-urban@outlook.com>
2024-01-28 19:54:15 +0100

    feat: lua tooling for hot restarting lsp via keybinding

--- attach_lsp.lua
+++ attach_lsp.lua
@@ -1,13 +1,26 @@
-vim.lsp.start({
-	name = "cargotomllsp",
-	cmd = { "cargotomllsp" },
-	root_dir = vim.fs.dirname(vim.fs.find({ 'Cargo.toml' }, { upward = true })[1]),
-})
+local function start_client()
+	vim.lsp.start({
+		name = "cargotomllsp",
+		cmd = { "cargotomllsp" },
+		root_dir = vim.fs.dirname(vim.fs.find({ 'Cargo.toml' }, { upward = true })[1]),
+	})
+end
+
+local function stop_client()
+	vim.lsp.stop_client(vim.lsp.get_clients())
+end
+
+local function restart_client()
+	stop_client()
+	start_client()
+end
+
+start_client()
 
 vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(args)
 		-- print(vim.inspect(args))
 		-- vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = args.buf })
-		vim.keymap.set('n', '<C-k>', vim.cmd [[ LspRestart ]], { buffer = args.buf })
+		vim.keymap.set('n', '<C-k>', restart_client, {})
 	end,
 })
